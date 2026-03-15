2f335fa708043601c6fd1831f05d849f3c32e33c
jumpfool <hk1337@proton.me>
2026-03-15 17:58:23 +0100

    fix

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -11,7 +11,8 @@ watch() {
       if [ "$current_sum" != "$last_sum" ]; then
         if nginx -t 2>&1; then
           nginx -s reload 2>&1
-          echo "nginx reload applied"
+          origin_line="$(grep -m1 "proxy_pass " /shared_nginx/waf.conf | tr -d ';')"
+          echo "nginx reload applied ${origin_line}"
           last_sum="$current_sum"
         else
           echo "nginx reload skipped (invalid config)"
