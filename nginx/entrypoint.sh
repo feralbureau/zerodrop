0a71c7303bdf7b2819bb0b64e1efde5b2ba7bffe
jumpfool <hk1337@proton.me>
2026-03-15 17:54:49 +0100

    fix: poller

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,8 +4,21 @@ set -e
 touch /shared_nginx/waf.conf
 
 watch() {
-  while inotifywait -e close_write,move,create /shared_nginx/waf.conf >/dev/null 2>&1; do
-    nginx -s reload >/dev/null 2>&1
+  last_sum=""
+  while true; do
+    if [ -f /shared_nginx/waf.conf ]; then
+      current_sum="$(sha256sum /shared_nginx/waf.conf | awk '{print $1}')"
+      if [ "$current_sum" != "$last_sum" ]; then
+        if nginx -t 2>&1; then
+          nginx -s reload 2>&1
+          echo "nginx reload applied"
+          last_sum="$current_sum"
+        else
+          echo "nginx reload skipped (invalid config)"
+        fi
+      fi
+    fi
+    sleep 2
   done
 }
 
