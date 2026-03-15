1c114be8019bee71282f7f546b9678cc42e7c7c4
jumpfool <hk1337@proton.me>
2026-03-15 17:45:47 +0100

    fix: say waalhi bro

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,11 +2,10 @@
 set -e
 
 touch /shared_nginx/waf.conf
-apk add --no-cache inotify-tools >/dev/null
 
 watch() {
   while inotifywait -e close_write,move,create /shared_nginx/waf.conf >/dev/null 2>&1; do
-    nginx -s reload >/dev/null 2>&1 || true
+    nginx -s reload >/dev/null 2>&1
   done
 }
 
